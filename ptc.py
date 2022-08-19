cd44e500016b79f7e4907000dbe14aea7a8e5f0f
12vishalkumar <90981381+12vishalkumar@users.noreply.github.com>
2022-08-20 00:07:48 +0530

    Add files via upload

---
+++ ptc.py
@@ -0,0 +1,24 @@
+import math
+import os
+import random
+import re
+import sys
+# Complete the 'pangrams' function below.
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+def pangrams(s):
+    # Write your code here
+    S = ''
+    for i in s.lower():
+        if i not in S:
+            S += i
+    if(len(S) > 26):
+        return 'pangram'
+    else:
+        return 'not pangram'
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    s = input()
+    result = pangrams(s)
+    fptr.write(result + '\n')
+    fptr.close()
